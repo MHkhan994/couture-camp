b4a9bce79d315c16c7c68296db2b6dc0f0351469
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2023-06-08 12:50:10 +0600

    instructor cards

--- src/Pages/Instructors/InstructorCard.jsx
+++ src/Pages/Instructors/InstructorCard.jsx
@@ -1,11 +1,17 @@
-
+import Aos from 'aos';
+import { useEffect } from "react";
+import 'aos/dist/aos.css';
 import './Instructors.css'
 
 const InstructorCard = ({ item }) => {
-    const { name, image, email } = item;
+    const { name, image, email, classNames } = item;
+
+    useEffect(() => {
+        Aos.init()
+    }, [])
 
     return (
-        <div className="card border shadow-md">
+        <div data-aos="fade-up" data-aos-duration="1000" className="card border shadow-md">
             <div className="image-container">
                 <img src={image} alt="Instructor" className="rounded-lg" />
                 <div className="overlay">
@@ -13,7 +19,15 @@ const InstructorCard = ({ item }) => {
                 </div>
             </div>
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
+                <h2 className="text-2xl font-bold">{name}</h2>
+                <div>
+                    <p className='text-lg font-semibold'>Classes:</p>
+                    {
+                        classNames.map((name, index) => <p className='ps-4' key={name}>
+                            <span className='text-[#03e9a4] pe-1'>{index + 1}.</span> {name}
+                        </p>)
+                    }
+                </div>
             </div>
         </div>
     );
